33064a895b0e52aed19609470cee2f025efcba52
Zavyar Tajdar <zavyar.tajdar100@gmail.com>
2025-08-29 01:06:45 +0500

    MongoDb Connect

---
+++ src/index.js
@@ -0,0 +1,49 @@
+// require("dotenv").config({path: "./env"});
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config();
+
+const app = express();
+
+// connect database
+connectDB();
+
+app.listen(process.env.PORT || 8000, () => {
+  console.log(`🚀 Server running on port ${process.env.PORT || 8000}`);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express";
+const app = express();
+( async ()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`, {
+           app.on('error', (error) => {
+               console.error("MongoDB connection error:", error);
+               throw error;
+           }),
+
+           app.listen(process.env.PORT, () => {
+               console.log(`Server is running on port ${process.env.PORT}`);
+           })
+        });
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+    }
+})();*/
