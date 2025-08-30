f81ed1604389d68c19ef9b80ccc468c40d22d74a
Zavyar Tajdar <zavyar.tajdar100@gmail.com>
2025-08-31 01:34:31 +0500

    User and Video Model and jwt

---
+++ src/models/user.models.js
@@ -0,0 +1,88 @@
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken"; // jwt is a bearer token means it`s like a key for accessing data  
+import bcrypt from "bcrypt";
+const UserSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true, //  It ignores whitespace
+        index: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    fullname:{
+        type: String,
+        required: true,
+        lowercase: true,
+        trim: true,
+        index: true
+    },
+    avatar:{
+        type: String, // Cloudinary url
+        required: true
+    },
+    cover:{
+        type: String, // Cloudinary url
+    },
+    watchHistory:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Video"
+    }],
+    password:{
+        type: String,
+        required: [true, "Password is required"],
+        minlength: [6, "Password must be at least 6 characters long"],
+        select: false
+    },
+    refreshToken:{
+        type: String
+    }
+}, {timestamps: true});
+
+UserSchema.pre("save", async function(next) {
+    if (!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+})
+
+UserSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password);
+}
+
+UserSchema.methods.generateAccessToken = async function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            username: this.username,
+            email: this.email,
+            fullname: this.fullname
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        { 
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+UserSchema.methods.generateRefreshToken = async function(){
+     return jwt.sign(
+        {
+            _id: this._id,
+            username: this.username,
+            email: this.email,
+            fullname: this.fullname
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        { 
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+export const User = mongoose.model("User", UserSchema);
---
+++ src/models/video.models.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+const VideoSchema = new mongoose.Schema({
+    videoFile:{
+        type: String, // Cloudinary url
+        required: true
+    },
+    thumbnail:{
+        type: String, // Cloudinary url
+        required: true
+    },
+    title:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    description:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    duration:{
+        type: Number,  // cloudinary duration
+        required: true
+    },
+    views:{
+        type: Number,
+        default: 0
+    },
+    likes:{
+        type: Number,
+        default: 0
+    },
+    isPublished:{
+        type: Boolean,
+        default: true
+    },
+    creator:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    category:{
+        type: String,
+        required: true
+    }
+},{timestamps: true})
+
+VideoSchema.plugin(mongooseAggregatePaginate);
+
+export const Video = mongoose.model("Video", VideoSchema);
