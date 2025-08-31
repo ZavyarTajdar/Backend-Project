d2704f75fc468d85c913896b483b2161d40aa45c
Zavyar Tajdar <zavyar.tajdar100@gmail.com>
2025-09-01 01:38:12 +0500

    Multer and cloudinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import {v2 as cloudinary} from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (filepath) =>{
+    try {
+        if (!filepath) return null;
+
+        // upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(filepath, {
+            resource_type: "auto",
+        })
+        // file have been uploaded successfully
+        console.log("File uploaded successfully" , response.url);
+        return response;
+    } catch (error) {
+        fs.unlinkSync(filepath); // remove the locally save tempapory file if operation got failed
+        console.error("Error uploading file to Cloudinary:", error);
+        return null;
+    }
+}
+export { uploadOnCloudinary }
